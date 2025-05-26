07e45ed2303ea960e0885b79b0d3115fa79462d5
Fabri-simo <joquin.simonian@mi.unc.edu.ar>
2025-05-25 22:33:44 -0300

    Update app.s
    Pintar fondo

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -1,11 +1,5 @@
-	.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
-	.equ BITS_PER_PIXEL,  	32
-
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
-
+	.include "size.s"
+	.include "shapes.s"
 	.globl main
 
 main:
@@ -13,19 +7,19 @@ main:
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
 	//---------------- CODE HERE ------------------------------------
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+	movz x10, 0x1B, lsl 16
+	movk x10, 0x3C42, lsl 00
+	bl backgroundColor
 
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+	mov x1, 0
+	mov x2, 240
+	mov x3, 640
+	mov x4, 240
+	movz x10, 0x1B, lsl 16
+	movk x10, 0x3235, lsl 00
+	bl rectangle
+
+
 
 	// Ejemplo de uso de gpios
 	mov x9, GPIO_BASE
